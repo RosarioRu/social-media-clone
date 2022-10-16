3c542e80b5320423eb6cea18cd1a7cf5519edc0a
RosarioRu <mariarruval@gmail.com>
2022-10-16 10:48:16 -0700

    Remove external stylesheet and add inline styling by using an object literal in the Header () function itself.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,25 @@
 import React from "react";
-import "./../Header.modules.css";
+// import "./../Header.modules.css";
 
 
 
 function Header() {
-  return(
-    <h1>El Face</h1>
   
+  const headerStyles = {
+    h1: {
+      textAlign: "center",
+      fontFamily: "comic 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif",
+      color: "rgb(144, 136, 136)"
+    },
+    h3: {
+      textAlign: "center"
+    }
+  }
+  return(
+    <React.Fragment>
+    <h1 style={headerStyles.h1}>El Face</h1>
+    <h3 style={headerStyles.h3}>Test</h3>
+    </React.Fragment>
   );
 }
 
