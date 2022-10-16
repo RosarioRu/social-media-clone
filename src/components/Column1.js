68f09fd05794bb6446c638bb2f2f55071ad01f27
RosarioRu <mariarruval@gmail.com>
2022-10-16 10:58:54 -0700

    Add styling to the column components.

--- src/components/Column1.js
+++ src/components/Column1.js
@@ -2,8 +2,16 @@ import React from "react";
 
 
 function Column1() {
+
+  const columnStyles = {
+    border: "solid black 2px",
+    display: "inline-block",
+    width: "33%",
+    textAlign: "Center"
+  }
+
   return(
-    <h3>Column 1</h3>
+    <h3 style={columnStyles}>Column 1</h3>
   );
 }
 
--- src/components/Column3.js
+++ src/components/Column3.js
@@ -2,8 +2,16 @@ import React from "react";
 
 
 function Column3() {
+
+  const columnStyles = {
+    border: "solid black 2px",
+    display: "inline-block",
+    width: "33%",
+    textAlign: "Center"
+  }
+
   return(
-    <h3>Column 3</h3>
+    <h3 style={columnStyles}>Column 3</h3>
   );
 }
 
